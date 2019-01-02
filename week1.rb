ebcf338835eec7193f09b6aadc164c1e87110f28
“Joe <joseph.sherman@Loop.colum.edu>
2019-01-02 14:03:29 -0600

    Add question 2. Wrote method called double_numbers that takes an array returning a new array with each number doubled. Used each loop.

--- week1.rb
+++ week1.rb
@@ -1,13 +1,27 @@
 # Question 1
-numbers = [1, 2, 4, 2]
-sum = 0
-index = 0
-numbers.each do |variable|
-  number = numbers[index] # number = 1, 2, 4, 2
-  sum = sum + number # sum = 1, 3, 7, 9
-  index += 1
+# numbers = [1, 2, 4, 2]
+# sum = 0
+# index = 0
+# numbers.each do |variable|
+#   number = numbers[index] # number = 1, 2, 4, 2
+#   sum = sum + number # sum = 1, 3, 7, 9
+#   index += 1
+# end
+# p sum
+
+# Question 2
+def double_numbers(input)
+  index = 0
+  array = []
+  input.each do
+    x = input[index] * 2
+    array << x
+    index +=1
+  end
+  p array
 end
-p sum
+double_numbers([2, 3, 7, 14])
+
 
 
 
